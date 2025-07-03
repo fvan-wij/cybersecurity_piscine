22073195cdb02afa36ffba0732dc830aa71e005f
fvan-wij <vanwijkflip@gmail.com>
2025-07-03 14:57:55 +0200

    Recursive shenanigans

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -3,6 +3,7 @@ import re
 import os
 import urllib.request
 from urllib.parse import urlparse
+import requests;
 from html.parser import HTMLParser
 import webbrowser
 
@@ -82,60 +83,109 @@ def print_settings():
     print("Path: ", arg_path)
     print("URL: ", arg_url)
 
-# def get_href(tuple):
-#     [item for item in a if item[0] == 1]
+
+path_set = set();
+
+def is_valid_extension(extension):
+    match extension:
+        case ".png":
+            return True
+        case ".jpg":
+            return True
+        case ".jpeg":
+            return True
+        case ".gif":
+            return True
+        case ".bmp":
+            return True
+        case _:
+            return False
 
 class MyHtmlParser(HTMLParser):
 
     def __init__(self):
         super().__init__()
-        self.url = None
+        self.netloc = None
+        self.current_path = None
+        self.images = set()
+        self.links = set()
 
-    def handle_starttag(self, tag, attrs): # for <a href="" ></a>
+    def handle_starttag(self, tag, attrs): # for </element>
         match tag:
             case "a":
-                # print("Encountered a link: ", tag)
                 attr_dict = dict(attrs)
                 link = attr_dict['href']
-                if link != None and link.find(url.netloc) == -1:
-                    print(link)
-                # if link != None and link.find(url.netloc) == len(url.netloc):
-                #     print(link)
+                if link != None and link.find(self.netloc) == -1:
+                    self.links.add(link)
+            case "img":
+                pair = os.path.splitext(attrs[0][1])
+                if is_valid_extension(pair[1]):
+                    # print(f"{self.netloc}/{attrs[0][1]}")
+                    self.images.add(f"{attrs[0][1]}")
 
-                # print(attr_dict['href'])
-                # print(attrs)
-                # for attr in attrs:
-                    # if attr.find(url.netloc):
-                    #     print(attr)
-                # if attrs.find(url.netloc):
-                    # print("Attributes: ", attrs)
-            # case "img":
-            #     print("Encountered an image: ", tag)
-            #     print("Attributes: ", attrs)
+    def get_images(self):
+        return self.images
 
-    # def handle_startendtag(self, tag, attrs): # for <img src=""/>
+    def get_links(self):
+        return self.links
+
+    # def handle_startendtag(self, tag, attrs): # for <element/>
     #     match tag:
     #         case "img":
     #             print("Encountered an image: ", tag)
     #             print("Attributes: ", attrs)
+    #
+
+def read_and_feed_parser(parser, url):
+    fp = urllib.request.urlopen(url) # Opens URL and sends request
+    bytes_read = fp.read()
+    fp.close()
+    html = bytes_read.decode("utf8")
+    parser.feed(html)
+
+def init_html_parser(netloc):
+    parser = MyHtmlParser()
+    return parser
+
+def get_images_till_depth_lvl(depth, base_url, path):
+    url = f"{base_url}/{path}"
+    if depth <= 0: 
+        return
+
+    print(f"Getting images for lvl {depth} on page: {base_url}/{path}")
+
+    parsed_url = urlparse(url)
+    parser = MyHtmlParser()
+    parser.netloc = parsed_url.netloc
+    parser.current_path = parsed_url.path
+    fp = urllib.request.urlopen(url)
+    mybytes = fp.read()
+    fp.close()
+    mystr = mybytes.decode("utf8")
+    parser.feed(mystr)
+    images = parser.get_images()
+    links = parser.get_links()
+    # with open("demofile.txt", "a") as f:
+        # f.write(f"LEVEL: {depth} | LINK: {base_url}{path}\n")
+    for img in images:
+
+        # print(f"Image: {img}")
+        # f.write(f"{img}\n")
+        # f.close()
+    for link in links:
+        # print(f"Link: {link}")
+        get_images_till_depth_lvl(depth - 1, base_url, link)
+
 
 try: # PARSING & VALIDATING INPUT
     parse_arguments()
     validate_arguments(arg_path, arg_url)
-    url = urlparse(arg_url)
-    print(url)
     print_settings()
 
-    # PARSING HTML IMG
-    fp = urllib.request.urlopen(arg_url)
-    mybytes = fp.read()
-    mystr = mybytes.decode("utf8")
-    fp.close()
-    parser = MyHtmlParser()
-    parser.url = url
-    parser.feed(mystr)
-    
-
+    # PARSING HTML/IMG
+    parsed_url = urlparse(arg_url)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    get_images_till_depth_lvl(arg_depth_level, base_url, parsed_url.path)
 
 except Exception as e:
     print("Error: ", e)
